ef8b0afd013aae6380dbbf83b82b21285ad00923
Jurijs Melniks <jurijs.melniks@28stone.com>
2023-02-20 20:05:07 +0200

    create methods for jdbc task 1 and jdbc task 2

--- src/main/java/ee/sda/Main.java
+++ src/main/java/ee/sda/Main.java
@@ -8,6 +8,11 @@ public class Main {
     public static final String DATABASE_PASSWORD = "Nomansland22";
 
     public static void main(String[] args) {
+        //firstExercise();
+        secondExercise();
+    }
+
+    private static void firstExercise() {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
@@ -18,21 +23,44 @@ public class Main {
                     DATABASE_USERNAME,
                     DATABASE_PASSWORD);
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM departments");
+            rs = stmt.executeQuery("SELECT * FROM projects");
 
             while(rs.next()) {
-                Integer deptId = rs.getInt("departmentId");
-                String deptName = rs.getString("name");
-
-                System.out.println(deptId + " " + deptName);
+                Integer projectId = rs.getInt("projectId");
+                String projectDescription = rs.getString("description");
+                System.out.println(projectId + " " + projectDescription);
             }
 
         } catch (SQLException sqlexc) {
             System.out.println("Database communication error");
             sqlexc.printStackTrace();
-        } finally {
-
         }
+    }
 
+    private static void secondExercise() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(
+                    DATABASE_HOST,
+                    DATABASE_USERNAME,
+                    DATABASE_PASSWORD);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM employees");
+
+            while(rs.next()) {
+                Integer employeeId = rs.getInt("employeeId");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                Date dateOfBirth = rs.getDate("dateOfBirth");
+                System.out.println(employeeId + " " + firstName + " " + lastName + " " + dateOfBirth);
+            }
+
+        } catch (SQLException sqlexc) {
+            System.out.println("Database communication error");
+            sqlexc.printStackTrace();
+        }
     }
 }
