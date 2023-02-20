9c32d313f45ced5cf8ce94ec2ea27a9a11a5c6ed
Jurijs Melniks <jurijs.melniks@28stone.com>
2023-02-20 19:34:59 +0200

    add our first mysql jdbc call

--- src/main/java/ee/sda/Main.java
+++ src/main/java/ee/sda/Main.java
@@ -1,7 +1,38 @@
 package ee.sda;
 
+import java.sql.*;
+
 public class Main {
+    public static final String DATABASE_HOST = "jdbc:mysql://localhost:3306/humanresources"; //JDBC URL
+    public static final String DATABASE_USERNAME = "root";
+    public static final String DATABASE_PASSWORD = "Nomansland22";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(
+                    DATABASE_HOST,
+                    DATABASE_USERNAME,
+                    DATABASE_PASSWORD);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM departments");
+
+            while(rs.next()) {
+                Integer deptId = rs.getInt("departmentId");
+                String deptName = rs.getString("name");
+
+                System.out.println(deptId + " " + deptName);
+            }
+
+        } catch (SQLException sqlexc) {
+            System.out.println("Database communication error");
+            sqlexc.printStackTrace();
+        } finally {
+
+        }
+
     }
 }
