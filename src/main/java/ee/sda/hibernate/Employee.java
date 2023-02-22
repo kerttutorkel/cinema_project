59d7356e7c80f207ecc4dbaea6cfc08501aaf504
Jurijs Melniks <jurijs.melniks@28stone.com>
2023-02-22 19:47:28 +0200

    adjust add our first hibernate annotations

--- src/main/java/ee/sda/hibernate/Employee.java
+++ src/main/java/ee/sda/hibernate/Employee.java
@@ -1,10 +1,13 @@
 package ee.sda.hibernate;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Date;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -17,39 +20,4 @@ public class Employee {
     private List<Project> project;
     private Date birthDate;
 
-    public Integer getEmployeeId() {
-        return employeeId;
-    }
-
-    public Employee setEmployeeId(Integer employeeId) {
-        this.employeeId = employeeId;
-        return this;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Employee setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Employee setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public Employee setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-        return this;
-    }
 }
